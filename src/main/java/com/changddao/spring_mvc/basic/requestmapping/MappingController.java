005beefe8e7b640ea5cc9b7b7934afceff3e5df5
Changho Youn <younch8342@yu.ac.kr>
2023-11-25 21:40:16 +0900

    Initial commit

---
+++ src/main/java/com/changddao/spring_mvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,15 @@
+package com.changddao.spring_mvc.basic.requestmapping;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class MappingController {
+    @RequestMapping("/hello-basic")
+    public String helloBasic() {
+        log.info("helloBasic");
+        return "OK";
+    }
+}
